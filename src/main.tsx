e3ddd28b2830285e1f6b0a75ed8c6e1d3319e9c9
hitomi.ozasa <hitomi.ozasa@novel.jpn.com>
2026-07-02 12:28:13 +0900

    features: FE環境開発

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { Provider as ChakraUIProvider } from '@/components/ui/provider'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ChakraUIProvider>
+      <App />
+    </ChakraUIProvider>
+  </StrictMode>,
+)
