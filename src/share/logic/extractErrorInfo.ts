8bda447aaa3dcccbb3a6d9ae13ca3c2c55e19660
hitomi.ozasa <hitomi.ozasa@novel.jpn.com>
2026-07-29 18:28:21 +0900

    fix: format修正

--- src/share/logic/extractErrorInfo.ts
+++ src/share/logic/extractErrorInfo.ts
@@ -6,9 +6,7 @@ import { type ErrorResponse } from '@/services/internal/backend/v1/types/respons
 //
 // axios.isAxiosError(e): eが「axiosが投げたエラー」かどうかを判定する関数（型ガード）
 // ここでtrueになった場合、TypeScript上でもeがAxiosErrorとして扱えるようになる
-export const extractErrorInfo = (
-  e: unknown,
-): ErrorResponse| undefined => {
+export const extractErrorInfo = (e: unknown): ErrorResponse | undefined => {
   // e.response?.data: レスポンス自体が無い（通信できなかった等）場合はresponseがundefinedになるため、
   // ?.（オプショナルチェイニング）でエラーにならないようにしている
   if (!axios.isAxiosError<ErrorResponse>(e) || !e.response) {
